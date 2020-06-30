c0be347a146d821ec375dbafada6280a8459b3b9
MarijaShopova <shopova.marija@yahoo.com>
2020-06-30 23:42:50 +0200

    Product-catalog module

---
+++ product-catalog/src/test/java/mk/ukim/finki/emt/productcatalog/ProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.productcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
